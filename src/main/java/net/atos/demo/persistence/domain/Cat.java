a1ddab1604e35e912d3d2c51044bcf7d3a67b2a7
a800716 <a800716@DESKTOP-CLC1N27.lan>
2020-11-10 16:05:56 +0000

    added cat entity and basic project

---
+++ src/main/java/net/atos/demo/persistence/domain/Cat.java
@@ -0,0 +1,68 @@
+package net.atos.demo.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cat {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String breed;
+	
+	private int age;
+	
+	private String colour;
+	
+	private String size;
+	
+	//Constructor
+	public Cat() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	
+}
---
+++ src/test/java/net/atos/demo/SpringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
